79cf35155eabe33509c82c000dfb9ec5cec118aa
Elysia Nason and Satchel Grant <elysia+satchel@>
2016-11-16 16:19:20 -0800

    add binary google tree answer

--- TreeNums.java
+++ TreeNums.java
@@ -2,56 +2,55 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TreeNums {
-  public List<Integer> searcher(int h, List<Integer> integerList) {
-    List<Integer> answerList = new ArrayList<>();
-    for (int i = 0; i < integerList.size(); i++) {
-      answerList.add(0);
-    }
-    int headIndex = (int) (Math.pow(2.0,h)-1.0);
-    int hminus1 = (int) Math.pow(2.0,h-1);
-    int leftChildIndex = headIndex - hminus1;
-    int rightChildIndex = headIndex-1;
+  public static List<Integer> searcher(int h, List<Integer> integerList) {
+     List<Integer> answerList = new ArrayList<>();
+     for (int i = 0; i < integerList.size(); i++) {
+       answerList.add(0);
+     }
+     int headIndex = (int) (Math.pow(2.0,h)-1.0);
+     int hminus1 = (int) Math.pow(2.0,h-1);
+     int leftChildIndex = headIndex - hminus1;
+     int rightChildIndex = headIndex-1;
 
-    answerList = matchExists(integerList, headIndex, leftChildIndex, answerList);
-    answerList = matchExists(integerList, headIndex, rightChildIndex, answerList);
+     answerList = matchExists(integerList, headIndex, leftChildIndex, answerList);
+     answerList = matchExists(integerList, headIndex, rightChildIndex, answerList);
 
-    answerList = helpSearcher(leftChildIndex,h-1, integerList, answerList);
-    answerList = helpSearcher(rightChildIndex,h-1, integerList, answerList);
+     answerList = helpSearcher(leftChildIndex,h-1, integerList, answerList);
+     answerList = helpSearcher(rightChildIndex,h-1, integerList, answerList);
 
-    for(int i = 0; i < answerList.size(); i++) {
-      if(answerList.get(i).equals(0)) {
-        answerList.set(i, -1);
-      }
-    }
+     for(int i = 0; i < answerList.size(); i++) {
+       if(answerList.get(i).equals(0)) {
+         answerList.set(i, -1);
+       }
+     }
 
-    return answerList;
-  }
+     return answerList;
+   }
 
-  public List<Integer> matchExists (List<Integer> integerList, int parentIndex, int childIndex, List<Integer> answerList) {
-    for(int i = 0; i < integerList.size(); i++) {
-      if (integerList.get(i).equals(childIndex)) {
-        answerList.set(i, parentIndex);
-      }
-    }
-    return answerList;
-  }
+   public static List<Integer> matchExists (List<Integer> integerList, int parentIndex, int childIndex, List<Integer> answerList) {
+     for(int i = 0; i < integerList.size(); i++) {
+       if (integerList.get(i).equals(childIndex)) {
+         answerList.set(i, parentIndex);
+       }
+     }
+     return answerList;
+   }
 
-  public List<Integer> helpSearcher(int headIndex, int h, List<Integer> integerList, List<Integer> answerList) {
-    if(h > 1) {
-      int hminus1 = (int) Math.pow(2.0,h-1);
-      int leftChildIndex = headIndex - hminus1;
-      int rightChildIndex = headIndex-1;
+   public static List<Integer> helpSearcher(int headIndex, int h, List<Integer> integerList, List<Integer> answerList) {
+     if(h > 1) {
+       int hminus1 = (int) Math.pow(2.0,h-1);
+       int leftChildIndex = headIndex - hminus1;
+       int rightChildIndex = headIndex-1;
 
-      answerList = matchExists(integerList, headIndex, leftChildIndex, answerList);
-      answerList = matchExists(integerList, headIndex, rightChildIndex, answerList);
+       answerList = matchExists(integerList, headIndex, leftChildIndex, answerList);
+       answerList = matchExists(integerList, headIndex, rightChildIndex, answerList);
 
-      answerList = helpSearcher(leftChildIndex, h-1, integerList, answerList);
-      answerList = helpSearcher(rightChildIndex, h-1, integerList, answerList);
-      return answerList;
+       answerList = helpSearcher(leftChildIndex, h-1, integerList, answerList);
+       answerList = helpSearcher(rightChildIndex, h-1, integerList, answerList);
+       return answerList;
 
-    } else {
-      return answerList;
-    }
-
-  }
+     } else {
+       return answerList;
+     }
+   }
 }
