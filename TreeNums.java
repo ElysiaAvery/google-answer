3bb912568c15d3b10a7faf1619ac138af1a1e260
Elysia Nason and Satchel Grant <elysia+satchel@>
2016-11-16 16:36:33 -0800

    add altered answer

--- TreeNums.java
+++ TreeNums.java
@@ -2,10 +2,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TreeNums {
-  public static List<Integer> searcher(int h, List<Integer> integerList) {
-     List<Integer> answerList = new ArrayList<>();
-     for (int i = 0; i < integerList.size(); i++) {
-       answerList.add(0);
+  public static int[] searcher(int h, int[] integerList) {
+     int[] answerList = new int[integerList.length];
+     for (int i = 0; i < integerList.length; i++) {
+       answerList[i] = 0;
      }
      int headIndex = (int) (Math.pow(2.0,h)-1.0);
      int hminus1 = (int) Math.pow(2.0,h-1);
@@ -18,25 +18,25 @@ public class TreeNums {
      answerList = helpSearcher(leftChildIndex,h-1, integerList, answerList);
      answerList = helpSearcher(rightChildIndex,h-1, integerList, answerList);
 
-     for(int i = 0; i < answerList.size(); i++) {
-       if(answerList.get(i).equals(0)) {
-         answerList.set(i, -1);
+     for(int i = 0; i < answerList.length; i++) {
+       if(answerList[i]==0) {
+         answerList[i] = -1;
        }
      }
 
      return answerList;
    }
 
-   public static List<Integer> matchExists (List<Integer> integerList, int parentIndex, int childIndex, List<Integer> answerList) {
-     for(int i = 0; i < integerList.size(); i++) {
-       if (integerList.get(i).equals(childIndex)) {
-         answerList.set(i, parentIndex);
+   public static int[] matchExists (int[] integerList, int parentIndex, int childIndex, int[] answerList) {
+     for(int i = 0; i < integerList.length; i++) {
+       if (integerList[i]==childIndex) {
+         answerList[i] = parentIndex;
        }
      }
      return answerList;
    }
 
-   public static List<Integer> helpSearcher(int headIndex, int h, List<Integer> integerList, List<Integer> answerList) {
+   public static int[] helpSearcher(int headIndex, int h, int[] integerList, int[] answerList) {
      if(h > 1) {
        int hminus1 = (int) Math.pow(2.0,h-1);
        int leftChildIndex = headIndex - hminus1;
